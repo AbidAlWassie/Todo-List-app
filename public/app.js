23c32d57163f702e276c50921dff2bf0dd6aed32
Abid Al-Wasi <abidalwasi@outlook.com>
2020-12-14 21:42:18 +0600

    updated project

--- public/app.js
+++ public/app.js
@@ -1,14 +1,1 @@
-function callFunction(callout) {
-  callout();
-}
-
-var output1 = function () {
-  console.log("hi");
-}
-
-var output2 = function () {
-  console.log("howdy");
-}
-
-callFunction(output1);
-callFunction(output2);
+require("count");
---
+++ public/count.js
@@ -0,0 +1,6 @@
+var counter = function (arr) {
+  return "There are " + arr.length + " elements in this array";
+}
+
+console.log(counter(["Eric", "Alex", "Jane", "John"]));
+console.log(counter(["Eric", "Matt", "Jane", "John"]));
